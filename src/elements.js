1fa246114b0ef5dd2f0e413e80e0170ecbd5a697
mjsdev5 <mcbridejames196@gmail.com>
2020-06-26 10:17:29 -0300

    add elements from DOM to js

---
+++ src/elements.js
@@ -0,0 +1,9 @@
+const board = document.getElementById('board');
+const cells = document.querySelectorAll('cell');
+const winningMessage = document.querySelectorAll('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const exitButton = document.getElementById('exitButton');
+
+export {
+  board, cells, winningMessage, restartButton, exitButton,
+};
---
+++ src/main.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-alert */
+import * as elements from './elements.js';
+
+console.log(elements.form);
