07065f93ae2b7689c37bfa56744902e8ad2d340d
mjsdev5 <mcbridejames196@gmail.com>
2020-06-29 10:12:39 -0300

    checkWin done

--- js/result.js
+++ js/result.js
@@ -2,7 +2,6 @@ const test = (result, array, index, x, y) => {
   result.push(array[index].classList[1]);
   result.push(array[index + x].classList[1]);
   result.push(array[index + y].classList[1]);
-  console.log(result);
 };
 
 const checkWinner = (result, ele) => {
@@ -14,22 +13,43 @@ const checkWinner = (result, ele) => {
     console.log('win');
   }
 };
+function checkRow(elem, index, array) {
+  const result = [];
+  if (index % 3 === 0) {
+    test(result, array, index, 1, 2);
+    checkWinner(result, array[index].classList[1]);
+  }
+}
+
+function checkColumn(elem, index, array) {
+  const result = [];
+  if (index < 3) {
+    test(result, array, index, 3, 6);
+    checkWinner(result, array[index].classList[1]);
+  }
+}
+
+function checkDiagonal(elem, index, array) {
+  const result = [];
+  if (index === 0) {
+    test(result, array, index, 4, 8);
+    checkWinner(result, array[index].classList[1]);
+  } else if (index === 2) {
+    test(result, array, index, 2, 4);
+    checkWinner(result, array[index].classList[1]);
+  }
+}
 
 function result() {
   const array = Array.from(document.querySelectorAll('.cell'));
-  let result = [];
 
   array.forEach((elem, index) => {
-    if (index % 3 === 0) {
-      test(result, array, index, 1, 2);
-      checkWinner(result, array[index].classList[1]);
-    } else if (index <= 2) {
-      test(result, array, index, 3, 6);
-      checkWinner(result, array[index].classList[1]);
-    }
-    result = [];
+    checkRow(elem, index, array);
+    checkColumn(elem, index, array);
+    checkDiagonal(elem, index, array);
   });
 }
+
 
 export default { result };
 
