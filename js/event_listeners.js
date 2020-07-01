df30fa8a57cde17a4f0aa33acd362b2047cfe54f
mjsdev5 <mcbridejames196@gmail.com>
2020-07-01 07:01:35 -0300

    remove inline comments

--- js/event_listeners.js
+++ js/event_listeners.js
@@ -5,9 +5,6 @@ import functions from './functions.js';
 import create from './player.js';
 
 const formSubmit = (input, parent, nextForm) => {
-  // const input = arrow.previousElementSibling;
-  // const nextForm = arrow.parentElement.nextElementSibling;
-
   if (input.type === 'text' && functions.validateUser(input)) {
     functions.nextSlide(parent, nextForm);
     if (elements.players.length === 0) {
@@ -29,23 +26,31 @@ const implement = () => {
   elements.restartButton.addEventListener('click', functions.restartGame);
   elements.exitButton.addEventListener('click', functions.exitGame);
 
-  elements.cells.forEach(elem => elem.addEventListener('click', (e) => { functions.populateBoard(e, elements.playerTurn); }));
+  elements.cells.forEach((elem) => elem.addEventListener('click', (e) => {
+    functions.populateBoard(e, elements.playerTurn);
+  }));
 
   elements.formInputs.forEach((input) => {
     input.addEventListener('keydown', (e) => {
       if (e.keyCode === 13) {
-        formSubmit(e.target, e.target.parentElement, e.target.parentElement.nextElementSibling);
+        formSubmit(
+          e.target,
+          e.target.parentElement,
+          e.target.parentElement.nextElementSibling,
+        );
       }
     });
   });
 
   elements.arrows.forEach((arrow) => {
     arrow.addEventListener('click', () => {
-      formSubmit(arrow.previousElementSibling, arrow.parentElement,
-        arrow.parentElement.nextElementSibling);
+      formSubmit(
+        arrow.previousElementSibling,
+        arrow.parentElement,
+        arrow.parentElement.nextElementSibling,
+      );
     });
   });
 };
-
 
 export default { implement };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-alert */
 
-// import * as elements from './elements.js';
-// import create from './player.js';
+
 import eventListeners from './event_listeners.js';
 
 
-// const playerOne = create.newPlayer('Messi', 'X', 0, 0);
 eventListeners.implement();
